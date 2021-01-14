bc373eb2d70c9b8b6199e44b96691807b160feca
GodLuckMister <paet14@gmail.com>
2021-01-15 00:05:59 +0200

    add new file task-11.js

--- js/task-10.js
+++ js/task-10.js
@@ -219,27 +219,59 @@
 // poly('eggs and bacon'); // Poly is cooking eggs and bacon
 
 
-const orders = [
-  { email: 'solomon@topmail.ua', dish: 'Burger' },
-  { email: 'artemis@coldmail.net', dish: 'Pizza' },
-  { email: 'jacob@mail.com', dish: 'Taco' },
-];
+// const orders = [
+//   { email: 'solomon@topmail.ua', dish: 'Burger' },
+//   { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//   { email: 'jacob@mail.com', dish: 'Taco' },
+// ];
 
-// Пиши код ниже этой строки
+// // Пиши код ниже этой строки
 
-function composeMessage(position) {
+// function composeMessage(position) {
 
- return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+//  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
 
-}
+// }
 
 
 
   
- const messages = orders.map((currenValue, index, arr)  => {return composeMessage.call(orders[index],index + 1)  });
+//  const messages = orders.map((currenValue, index, arr)  => {return composeMessage.call(orders[index],index + 1)  });
 
-  console.log(messages);
+//   console.log(messages);
 
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    
+    this.mailingList.push(email);
+    
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
+}
+
+console.log(logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service)));
+// Почта kiwi@mail.uk добавлена в рассылку.
+
+console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+
+console.log(logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service)));
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
 
 
 
